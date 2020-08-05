940e8b7090e4f914ae8e9e4bb988653134d0ab0d
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-08-05 18:03:26 +0200

    prices component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <div id="header">
+      <p>
+        THIS IS A <span>HEADING</span>
+      </p>
+    </div>
+  );
+}
